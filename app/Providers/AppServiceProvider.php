123268ecf12d7f0768a266cfd00df54c57678d88
Raihan Parama <raihanpl10@gmail.com>
2021-11-12 00:59:35 +0700

    fix image issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(config('app.env') === 'production') {
+        if(env('APP_ENV') === 'production') {
             URL::forceScheme('https');
          }
     }
